5b2d1c593241b31676b13944a9b37f3409d6c322
sumitchowdhary <sumitchowdhary@matrixnmedia.com>
2024-05-02 21:39:45 +0530

    file chages

--- pages/_middleware/authenticate.ts
+++ pages/_middleware/authenticate.ts
@@ -6,9 +6,9 @@ interface Cookies {
 }
 
 export function authenticate(req: NextRequest) {
-  const cookies: Cookies = req.cookies;
+  const cookies: Cookies = req.cookies as Cookies; // Asserting req.cookies to be of type Cookies
 
-  if (cookies && cookies.id !== 'webskitters' || cookies.password !== 'webskitters') {
+  if (!cookies || cookies.id !== 'webskitters' || cookies.password !== 'webskitters') {
     return NextResponse.redirect('/login');
   }
 
